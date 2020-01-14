941e25cfe30aba619e6f9b8a4b2a4988a7fe4471
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 15:35:06 +0200

    Add project files.

---
+++ 19.uzd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _19.uzd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 100, 200, 233, 66, 77, 98 };
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[numbers.Length - 1]);
+
+        }
+    }
+}
+
+
+//Izveidot console aplikāciju, kurā ir definēts int masīvs ar vērtībām 100, 200, 233, 66, 77, 98.
+// Izdrukāt uz ekrāna pirmā un pēdējā elementa vērtības.
